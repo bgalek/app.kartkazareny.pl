c5dbc88111b441a35e67a395748fb676ddd3782a
Dawid Tomczak <dawid.tomczak@allegro.pl>
2022-04-09 17:07:03 +0200

    main wrapper div

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import styles from "./App.module.css";
+import Header from "./components/Header";
 import Layout from "./components/Layout";
 import { NeedsContextProvider } from "./contexts/NeedsContext";
 import { NeedFormManager } from "./components/NeedForm/NeedFormManager";
 import { AddedNeeds } from "./components/AddedNeeds/AddedNeeds";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { blueGrey } from '@mui/material/colors';
+import { blueGrey } from "@mui/material/colors";
 
 const theme = createTheme({
-    palette: {
-        primary: blueGrey
-    }
+  palette: {
+    primary: blueGrey,
+  },
 });
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <Header/>
-            <Layout>
-                <NeedsContextProvider>
-                    <NeedFormManager/>
-                    <AddedNeeds/>
-                </NeedsContextProvider>
-            </Layout>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={styles.wrapper}>
+        <Header />
+        <CssBaseline />
+        <Layout>
+          <NeedsContextProvider>
+            <NeedFormManager />
+            <AddedNeeds />
+          </NeedsContextProvider>
+        </Layout>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
