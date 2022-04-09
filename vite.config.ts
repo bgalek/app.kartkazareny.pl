782d106f883ad50262e7bdb794e446a55df0b916
Dawid Tomczak <dawid.tomczak@allegro.pl>
2022-04-09 17:05:37 +0200

    vite config for css modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactCssModule from "vite-plugin-react-css-modules";
+const generateScopedName = "[name]__[local]___[hash:base64:5]";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    reactCssModule({
+      generateScopedName,
+      filetypes: {
+        "module.css": {
+          syntax: "sugarss",
+        },
+      },
+    }),
+    reactRefresh(),
+  ],
+  css: {
+    modules: {
+      generateScopedName,
+    },
+  },
+});
