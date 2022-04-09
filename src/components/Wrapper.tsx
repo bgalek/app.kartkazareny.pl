701dbab28bfdde108bc72705808240ed115f47d0
dawid-tomczak <dawid@tomczakd.pl>
2022-04-09 22:11:11 +0200

    wrapper props fix

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -3,7 +3,6 @@ import styles from "./Wrapper.module.css";
 
 interface Props {
   children: ReactElement | ReactElement[];
-  className: string;
 }
 
 export const Wrapper = ({ children }: Props) => (
