f5afd55ddb2a153317fe59127997b5dcde918e0c
dawid-tomczak <dawid@tomczakd.pl>
2022-04-11 17:56:28 +0200

    added result snackbar

--- src/components/AddedNeeds/AddedNeeds.tsx
+++ src/components/AddedNeeds/AddedNeeds.tsx
@@ -1,11 +1,15 @@
 import React, { ReactElement, useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { NeedsContext } from "../../contexts/NeedsContext";
+import { SnackbarContext } from "../../contexts/SnackbarContext";
 import { postNeeds } from "../../helpers/ArenaAPI";
 import { NeedsList } from "./NeededsList/NeedsList";
 import { SendNeeds } from "./SubmitNeeds/SendNeeds";
 
 export const AddedNeeds = (): ReactElement => {
   const { needs, deleteNeed, resetNeeds } = useContext(NeedsContext);
+  const { showSnackbar } = useContext(SnackbarContext);
+  const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSend = async (): Promise<void> => {
@@ -15,11 +19,13 @@ export const AddedNeeds = (): ReactElement => {
       await postNeeds(needs);
     } catch {
       setIsLoading(false);
+      showSnackbar(t("Coś poszło nie tak, spróbuj ponownie za chwilę"), true);
       return;
     }
 
     resetNeeds();
     setIsLoading(false);
+    showSnackbar(t("Zapotrzebowanie wysłane!"));
   };
 
   return (
