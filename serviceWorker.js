d2a41c141428360c399da22f205c6f66351a2a65
dawid-tomczak <dawid@tomczakd.pl>
2022-04-16 12:52:12 +0200

    no-op service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,20 @@
-let CACHE_NAME = "app-kartkazareny-cache";
-const urlsToCache = ["/", "/index.html"];
+// A simple, no-op service worker that takes immediate control.
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request);
-    })
-  );
+self.addEventListener("activate", () => {
+  // Optional: Get a list of all the current open windows/tabs under
+  // our service worker's control, and force them to reload.
+  // This can "unbreak" any open windows/tabs as soon as the new
+  // service worker activates, rather than users having to manually reload.
+  self.clients.matchAll({ type: "window" }).then((windowClients) => {
+    windowClients.forEach((windowClient) => {
+      windowClient.navigate(windowClient.url);
+    });
+  });
 });
